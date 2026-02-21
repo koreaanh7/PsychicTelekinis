b6b14b81a098a18aab517a25ebb714df083263f4
koreaanh7 <koreaanh7@gmail.com>
2026-02-21 13:38:03 +0700

    Delete const {join} = require('path');  /**  * @type {import("puppeteer").Configuration}  */ module.exports = {    directory

--- const {join} = require('path');  /**  * @type {import("puppeteer").Configuration}  */ module.exports = {   /.puppeteerrc.cjs
+++
@@ -1,9 +0,0 @@
-const {join} = require('path');
-
-/**
- * @type {import("puppeteer").Configuration}
- */
-module.exports = {
-  // Ép thư mục cache của Puppeteer nằm ngay trong thư mục code
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-};
